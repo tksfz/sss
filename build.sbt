afb83e5c8d5044e68b741a2b5bcc495a9b95657f
thom <thomkim+github@gmail.com>
2012-05-20 11:46:52 -0700

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "sss"
+
+version := "0.1"
+
+scalaVersion := "2.9.1"
---
+++ src/main/scala/org/tksfz/sss/Sss.scala
@@ -0,0 +1,11 @@
+package org.tksfz.sss
+
+class Sss {
+
+}
+
+object Sss {
+  def main(args: Array[String]) = {
+    val scriptFilename = args(0)
+  }
+}
