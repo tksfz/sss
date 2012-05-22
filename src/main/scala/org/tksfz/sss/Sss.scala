2563ad337710df837ac32c1195897761f2985625
thom <thomkim+github@gmail.com>
2012-05-21 20:53:43 -0700

    wow it works

--- src/main/scala/org/tksfz/sss/Sss.scala
+++ src/main/scala/org/tksfz/sss/Sss.scala
@@ -2,19 +2,25 @@ package org.tksfz.sss
 
 import com.twitter.util.Eval
 import java.io.File
+import scala.io.Source
 
 class Sss(
   scriptFilename: String
 ) {
   def run = {
+    var scriptLines = Source.fromFile(scriptFilename).getLines
+    scriptLines = scriptLines.dropWhile { _.startsWith("#") }
+    val script = scriptLines.mkString("\n")
     val eval = new Eval
-    eval(new File(scriptFilename))
+    eval(script)
     // strip the #
+    ""
   }
 }
 
 object Sss {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]) {
     val scriptFilename = args(0)
+    new Sss(scriptFilename).run    
   }
 }
